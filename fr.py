6b4ff39b62aed69c37c69d9b6b05f204a0ae4877
Ferz <168175684+ferdinandderosaputra@users.noreply.github.com>
2024-09-09 01:45:33 +0700

    Add files via upload

--- fr.py
+++ fr.py
@@ -1,32 +1,51 @@
 import os
-import platform
+try:
+    import distro
+except ImportError:
+    import platform
 
 def clear_terminal():
     os.system('clear')
 
-def custom_text():
-    print("""
+def custom_text(distro_name):
+    print(f"""
             ┏━╸┏━┓┏━┓╺┳╸┏━╸┏━┓   ┏━┓┏━╸┏━┓┏━┓┏━┓╻╺┳╸┏━┓┏━┓╻ ╻
             ┣╸ ┣━┫┗━┓ ┃ ┣╸ ┣┳┛   ┣┳┛┣╸ ┣━┛┃ ┃┗━┓┃ ┃ ┃ ┃┣┳┛┗┳┛
             ╹  ╹ ╹┗━┛ ╹ ┗━╸╹┗╸   ╹┗╸┗━╸╹  ┗━┛┗━┛╹ ╹ ┗━┛╹┗╸ ╹ 
         ---------------------------------------------------------------
-        |Creator: FerzChills                                          |
+        |Creator: FerdinandDeroSaputra                                |
         |Inspired By: frozzipies                                      |
         |    This Will Make Updating and Upgrading Your Linux         |
         |                  repository 100% faster                     |
         ---------------------------------------------------------------
+        You are currently using {distro_name}.
 """)
 
 def get_distro():
-    dist_name = platform.linux_distribution()[0].lower()
-    if 'ubuntu' in dist_name:
-        return 'ubuntu'
-    elif 'debian' in dist_name:
-        return 'debian'
-    elif 'kali' in dist_name:
-        return 'kali'
-    else:
-        return 'unknown'
+    try:
+        dist_name = distro.name().lower()
+        if 'ubuntu' in dist_name:
+            return 'ubuntu'
+        elif 'debian' in dist_name:
+            return 'debian'
+        elif 'kali' in dist_name:
+            return 'kali'
+        else:
+            return 'unknown'
+    except NameError:
+        dist_name = platform.dist()[0].lower()
+        if 'ubuntu' in dist_name:
+            return 'ubuntu'
+        elif 'debian' in dist_name:
+            return 'debian'
+        elif 'kali' in dist_name:
+            return 'kali'
+        else:
+            return 'unknown'
+
+def show_distro_info():
+    distro_name = get_distro()
+    print(f"Detected Linux Distribution: {distro_name.capitalize()}")
 
 def handle_repo_for_distro(distro, action):
     repo_lines = {
@@ -81,25 +100,26 @@ def handle_repo_for_distro(distro, action):
 def update_and_upgrade_repository():
     os.system('sudo apt update -o Acquire::http::Pipeline-Depth=0 && sudo apt upgrade -y --fix-missing')
     clear_terminal()
-    custom_text()
+    custom_text(get_distro())
     print("Repository has been updated and upgraded.")
 
 def revert_to_old_repository():
     if os.path.isfile('/etc/apt/sources.list.bak'):
         os.system('cp /etc/apt/sources.list.bak /etc/apt/sources.list')
         clear_terminal()
-        custom_text()
+        custom_text(get_distro())
         print("Repository has been reverted to the old configuration from sources.list.bak.")
     else:
         clear_terminal()
-        custom_text()
+        custom_text(get_distro())
         print("No backup of the original sources.list file found. Cannot revert.")
 
 def get_user_choice():
-    distro = get_distro()
+    distro_name = get_distro()
+    show_distro_info()
     
     while True:
-        print("Choose an option:")
+        print("\nChoose an option:")
         print("1. Speed up repository update & upgrade")
         print("2. Add repository (for non-default repos)")
         print("3. Update & Upgrade Repository")
@@ -108,9 +128,9 @@ def get_user_choice():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            handle_repo_for_distro(distro, 'speed_up')
+            handle_repo_for_distro(distro_name, 'speed_up')
         elif choice == '2':
-            handle_repo_for_distro(distro, 'add')
+            handle_repo_for_distro(distro_name, 'add')
         elif choice == '3':
             update_and_upgrade_repository()
         elif choice == '4':
@@ -124,7 +144,7 @@ def get_user_choice():
             print("Invalid choice. Please try again.")
 
 clear_terminal()
-custom_text()
+custom_text(get_distro())
 
 while True:
     get_user_choice()
